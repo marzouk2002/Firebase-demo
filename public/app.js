19d57ebf6f2623561a9b72c7e61166d772195043
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-04-28 15:02:10 +0000

    hooking with the db

--- public/app.js
+++ public/app.js
@@ -1,1 +1,35 @@
-console.log(firebase)
+const auth = firebase.auth();
+
+const signedInSection =  document.querySelector('#signIn')
+const signedOutSection =  document.querySelector('#signOut')
+
+const signInBtn = document.querySelector('#signInBtn')
+const signOutBtn = document.querySelector('#signOutBtn')
+
+const userDetails = document.querySelector('#userDetails')
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+signInBtn.onclick = () => auth.signInWithPopup(provider)
+
+signOutBtn.onclick = () => auth.signOut()
+
+auth.onAuthStateChanged(user => {
+    if (user) {
+        signedInSection.hidden = false
+        signOut.hidden = true
+        userDetails.innerHTML = `<h3>Hello, ${user.displayName}</h3> <p>User ID: ${user.uid}</p>`
+    } else {
+        signedInSection.hidden = true
+        signOut.hidden = false
+        userDetails.innerHTML = ''
+    }
+})
+
+const store = firebase.firestore()
+
+const createBtn = document.querySelector('#createNewThing')
+const thingsList = document.querySelector('#listOfthings')
+
+let thingsRef;
+let unsubscribe;
